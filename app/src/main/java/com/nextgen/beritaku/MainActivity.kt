be2de641e9e3c10a166e8662594709092b754679
ribakmasude <beranjusihombing1@gmail.com>
2022-12-28 20:30:31 +0700

    setup bottom navigation

--- app/src/main/java/com/nextgen/beritaku/MainActivity.kt
+++ app/src/main/java/com/nextgen/beritaku/MainActivity.kt
@@ -2,10 +2,45 @@ package com.nextgen.beritaku
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.navigation.NavDestination
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.nextgen.beritaku.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val binding: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        // ** setup bottom navigation
+        val navHostFragment =supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController =navHostFragment.navController
+        binding.navView.setupWithNavController(navController)
+
+        navController.addOnDestinationChangedListener{_, destination: NavDestination, _ ->
+            when(destination.id){
+                R.id.home_navigation -> visible()
+                R.id.explore_navigation -> visible()
+                R.id.favorite_navigation -> visible()
+                R.id.account_navigation -> visible()
+                else -> invisible()
+            }
+        }
+
     }
+
+    private fun invisible() {
+        binding.navView.visibility = View.GONE
+    }
+
+    private fun visible() {
+        binding.navView.visibility = View.VISIBLE
+    }
+
+
 }
