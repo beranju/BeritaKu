d3ea56260ca06a9216859c85cf24cf336919ba48
beranju <beranjusihombing1@gmail.com>
2023-09-22 23:47:48 +0700

    update logo and splash screen

--- app/src/main/java/com/nextgen/beritaku/MainActivity.kt
+++ app/src/main/java/com/nextgen/beritaku/MainActivity.kt
@@ -21,11 +21,8 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
-
-        // ** Install splashscreen
-        installSplashScreen()
-
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
